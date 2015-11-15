ba5cfd11583575d9fa25e1475aa2055c9b0b9a87
Theo Browne <theo@theobrowne.com>
2015-11-14 22:28:29 -0500

    Basic website blocker

--- CourseLoaded/js/bootstrap.js
+++ CourseLoaded/js/bootstrap.js
@@ -20,10 +20,137 @@
     document.getElementById('time').innerHTML = time;
   }
 
-
   window.onload = function() {
     // Update the clock every second
     displayCurrentTime();
     setInterval(displayCurrentTime, 1000);
   };
 })();
+
+  function defaultPrefs() {
+  return {
+    siteList: [
+      'facebook.com',
+      'youtube.com',
+      'twitter.com',
+      'tumblr.com',
+      'pinterest.com',
+      'myspace.com',
+      'livejournal.com',
+      'digg.com',
+      'stumbleupon.com',
+      'reddit.com',
+      'kongregate.com',
+      'newgrounds.com',
+      'addictinggames.com',
+      'hulu.com'
+    ],
+    durations: { // in seconds
+      work: 25 * 60,
+      break: 5 * 60
+    },
+    shouldRing: true,
+    clickRestarts: false,
+    whitelist: false
+  }
+}
+
+var PREFS = defaultPrefs();
+
+function parseLocation(location) {
+  var components = location.split('/');
+  return {domain: components.shift(), path: components.join('/')};
+}
+
+function pathsMatch(test, against) {
+  /*
+    index.php ~> [null]: pass
+    index.php ~> index: pass
+    index.php ~> index.php: pass
+    index.php ~> index.phpa: fail
+    /path/to/location ~> /path/to: pass
+    /path/to ~> /path/to: pass
+    /path/to/ ~> /path/to/location: fail
+  */
+
+  return !against || test.substr(0, against.length) == against;
+}
+
+function domainsMatch(test, against) {
+  /*
+    google.com ~> google.com: case 1, pass
+    www.google.com ~> google.com: case 3, pass
+    google.com ~> www.google.com: case 2, fail
+    google.com ~> yahoo.com: case 3, fail
+    yahoo.com ~> google.com: case 2, fail
+    bit.ly ~> goo.gl: case 2, fail
+    mail.com ~> gmail.com: case 2, fail
+    gmail.com ~> mail.com: case 3, fail
+  */
+
+  // Case 1: if the two strings match, pass
+  if(test === against) {
+    return true;
+  } else {
+    var testFrom = test.length - against.length - 1;
+
+    // Case 2: if the second string is longer than first, or they are the same
+    // length and do not match (as indicated by case 1 failing), fail
+    if(testFrom < 0) {
+      return false;
+    } else {
+      // Case 3: if and only if the first string is longer than the second and
+      // the first string ends with a period followed by the second string,
+      // pass
+      return test.substr(testFrom) === '.' + against;
+    }
+  }
+}
+
+function locationsMatch(location, listedPattern) {
+  return domainsMatch(location.domain, listedPattern.domain) &&
+    pathsMatch(location.path, listedPattern.path);
+}
+
+function isLocationBlocked(location) {
+  for(var k in PREFS.siteList) {
+    listedPattern = parseLocation(PREFS.siteList[k]);
+    if(locationsMatch(location, listedPattern)) {
+      // If we're in a whitelist, a matched location is not blocked => false
+      // If we're in a blacklist, a matched location is blocked => true
+      return !PREFS.whitelist;
+    }
+  }
+  
+  // If we're in a whitelist, an unmatched location is blocked => true
+  // If we're in a blacklist, an unmatched location is not blocked => false
+  return PREFS.whitelist;
+}
+
+function executeInTabIfBlocked(action, tab) {
+  var file = "js/" + action + ".js", location;
+  location = tab.url.split('://');
+  location = parseLocation(location[1]);
+  
+  if(isLocationBlocked(location)) {
+    chrome.tabs.executeScript(tab.id, {file: file});
+  }
+}
+
+function executeInAllBlockedTabs(action) {
+  var windows = chrome.windows.getAll({populate: true}, function (windows) {
+    var tabs, tab, domain, listedDomain;
+    for(var i in windows) {
+      tabs = windows[i].tabs;
+      for(var j in tabs) {
+        executeInTabIfBlocked(action, tabs[j]);
+      }
+    }
+  });
+}
+
+executeInAllBlockedTabs('block');
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    executeInTabIfBlocked('block', tab);
+});
